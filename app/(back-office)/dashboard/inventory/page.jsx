6891eebb864406f083dca95666fe57a9df66ed51
Ermias <raserloup@gmail.com>
2024-10-04 18:19:23 +0300

    error fixing

--- app/(back-office)/dashboard/inventory/page.jsx
+++ app/(back-office)/dashboard/inventory/page.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Inventory() {
-  return (
-    <div>Inventory</div>
-  )
+  return <div>Inventory Creation Options</div>;
 }
